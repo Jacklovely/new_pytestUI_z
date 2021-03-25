f3370e578141f1a83a02ae5e3f6002e0be352360
Jacklovely / <hanxi978@163.com>
2021-03-25 10:45:29 +0800

    修改测试地址

--- case/test_add_equipment.py
+++ case/test_add_equipment.py
@@ -45,3 +45,32 @@ class Test_Add_Equipment():
             self.log.info("获取结果：%s"%result)
         with allure.step("断言：判断是否打开成功"):
             assert result
+
+    @allure.story("用例：添加设备")
+    @pytest.mark.parametrize("name,model,num,brand,version,msg", testdata["equipment_fail_data"],
+                             ids=["添加设备，断言失败"])
+    # @pytest.mark.skip('跳过该成功用例')
+    def test_add_equipment_2(self, login_fixtrue, name, model, num, brand, version,msg):
+        driver = login_fixtrue
+        register = Add_Equipment_Page(driver)
+        with allure.step("点击添加设备,跳转添加设备"):
+            time.sleep(1)
+            register.click_add_equipment()
+        with allure.step('输入设备名'):
+            time.sleep(1)
+            register.input_name(text=name)
+        with allure.step('输入型号'):
+            register.input_model(text=model)
+        with allure.step('输入编号'):
+            register.input_num(text=num)
+        with allure.step('输入品牌'):
+            register.input_brand(text=brand)
+        with allure.step('输入系统'):
+            register.input_version(text=version)
+        with allure.step('点击添加'):
+            register.click_add()
+        with allure.step("获取结果: 获取页面实际结果，判断是否注册成功"):
+            result = register.register_success(expect_text=msg)
+            self.log.info("获取结果：%s" % result)
+        with allure.step("断言：判断是否打开成功"):
+            assert result
